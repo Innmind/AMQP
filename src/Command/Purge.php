885090181328c3ddd54a6dce5ff7cec3a6d199f2
Baptiste Langlade <baptouuuu@gmail.com>
2018-02-23 08:09:28 +0100

    add purge command

---
+++ src/Command/Purge.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\AMQP\Command;
+
+use Innmind\AMQP\{
+    Client,
+    Model\Queue,
+    Exception\UnexpectedFrame,
+};
+use Innmind\CLI\{
+    Command,
+    Command\Arguments,
+    Command\Options,
+    Environment,
+};
+use Innmind\Immutable\Str;
+
+final class Purge implements Command
+{
+    private $client;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    public function __invoke(Environment $env, Arguments $arguments, Options $options): void
+    {
+        $queue = $arguments->get('queue');
+
+        try {
+            $this
+                ->client
+                ->channel()
+                ->queue()
+                ->purge(new Queue\Purge($queue));
+        } catch (UnexpectedFrame $e) {
+            $env->error()->write(Str::of("Purging \"$queue\" failed"));
+            $env->exit(1);
+        }
+    }
+
+    public function __toString(): string
+    {
+        return <<<USAGE
+innmind:amqp:purge queue
+
+Will delete all messages for the given queue
+USAGE;
+    }
+}
---
+++ tests/Command/PurgeTest.php
@@ -0,0 +1,114 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\AMQP\Command;
+
+use Innmind\AMQP\{
+    Command\Purge,
+    Client,
+    Client\Channel,
+    Client\Channel\Queue,
+    Exception\UnexpectedFrame,
+    Transport\Frame,
+};
+use Innmind\CLI\{
+    Command,
+    Command\Arguments,
+    Command\Options,
+    Environment,
+};
+use Innmind\Stream\Writable;
+use Innmind\Immutable\Map;
+use PHPUnit\Framework\TestCase;
+
+class PurgeTest extends TestCase
+{
+    public function testInterface()
+    {
+        $this->assertInstanceOf(
+            Command::class,
+            new Purge($this->createMock(Client::class))
+        );
+    }
+
+    public function testDefinition()
+    {
+        $expected = <<<USAGE
+innmind:amqp:purge queue
+
+Will delete all messages for the given queue
+USAGE;
+
+        $this->assertSame($expected, (string) new Purge($this->createMock(Client::class)));
+    }
+
+    public function testInvokation()
+    {
+        $purge = new Purge($client = $this->createMock(Client::class));
+        $client
+            ->expects($this->once())
+            ->method('channel')
+            ->willReturn($channel = $this->createMock(Channel::class));
+        $channel
+            ->expects($this->once())
+            ->method('queue')
+            ->willReturn($queue = $this->createMock(Queue::class));
+        $queue
+            ->expects($this->once())
+            ->method('purge')
+            ->with($this->callback(function($purge): bool {
+                return $purge->name() === 'foo';
+            }));
+        $env = $this->createMock(Environment::class);
+        $env
+            ->expects($this->never())
+            ->method('exit');
+
+        $this->assertNull($purge(
+            $env,
+            new Arguments((new Map('string', 'mixed'))->put('queue', 'foo')),
+            new Options
+        ));
+    }
+
+    public function testFailsWhenUnexpectedFrame()
+    {
+        $purge = new Purge($client = $this->createMock(Client::class));
+        $client
+            ->expects($this->once())
+            ->method('channel')
+            ->willReturn($channel = $this->createMock(Channel::class));
+        $channel
+            ->expects($this->once())
+            ->method('queue')
+            ->willReturn($queue = $this->createMock(Queue::class));
+        $queue
+            ->expects($this->once())
+            ->method('purge')
+            ->with($this->callback(static function($purge): bool {
+                return $purge->name() === 'foo';
+            }))
+            ->will($this->throwException(new UnexpectedFrame(Frame::heartbeat())));
+        $env = $this->createMock(Environment::class);
+        $env
+            ->expects($this->once())
+            ->method('exit')
+            ->with(1);
+        $env
+            ->expects($this->once())
+            ->method('error')
+            ->willReturn($error = $this->createMock(Writable::class));
+        $error
+            ->expects($this->once())
+            ->method('write')
+            ->with($this->callback(static function($str): bool {
+                return (string) $str === 'Purging "foo" failed';
+            }));
+
+        $this->assertNull($purge(
+            $env,
+            new Arguments((new Map('string', 'mixed'))->put('queue', 'foo')),
+            new Options
+        ));
+    }
+}
