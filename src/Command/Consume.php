c55dc6583cdbcd6e1d59db76130849d051ef27fb
Baptiste Langlade <baptouuuu@gmail.com>
2018-02-23 08:35:47 +0100

    add consume command

---
+++ src/Command/Consume.php
@@ -0,0 +1,59 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\AMQP\Command;
+
+use Innmind\AMQP\{
+    Client,
+    Consumers,
+    Model\Basic,
+    Model\Basic\Qos,
+};
+use Innmind\CLI\{
+    Command,
+    Command\Arguments,
+    Command\Options,
+    Environment,
+};
+
+final class Consume implements Command
+{
+    private $client;
+    private $consumers;
+
+    public function __construct(Client $client, Consumers $consumers)
+    {
+        $this->client = $client;
+        $this->consumers = $consumers;
+    }
+
+    public function __invoke(Environment $env, Arguments $arguments, Options $options): void
+    {
+        $queue = $arguments->get('queue');
+        $consume = $this->consumers->get($queue);
+        $basic = $this->client->channel()->basic();
+
+        if ($arguments->contains('prefetch')) {
+            $basic->qos(new Qos(0, (int) $arguments->get('prefetch')));
+        } else if ($arguments->contains('number')) {
+            $basic->qos(new Qos(0, (int) $arguments->get('number')));
+        }
+
+        $consumer = $basic->consume(new Basic\Consume($queue));
+
+        if ($arguments->contains('number')) {
+            $consumer->take((int) $arguments->get('number'));
+        }
+
+        $consumer->foreach($consume);
+    }
+
+    public function __toString(): string
+    {
+        return <<<USAGE
+innmind:amqp:consume queue [number] [prefetch]
+
+Will process messages from the given queue
+USAGE;
+    }
+}
---
+++ tests/Command/ConsumeTest.php
@@ -0,0 +1,211 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\AMQP\Command;
+
+use Innmind\AMQP\{
+    Command\Consume,
+    Client,
+    Client\Channel,
+    Client\Channel\Basic,
+    Consumers,
+};
+use Innmind\CLI\{
+    Command,
+    Command\Arguments,
+    Command\Options,
+    Environment,
+};
+use Innmind\Immutable\Map;
+use PHPUnit\Framework\TestCase;
+
+class ConsumeTest extends TestCase
+{
+    public function testInterface()
+    {
+        $this->assertInstanceOf(
+            Command::class,
+            new Consume(
+                $this->createMock(Client::class),
+                new Consumers
+            )
+        );
+    }
+
+    public function testDefinition()
+    {
+        $expected = <<<USAGE
+innmind:amqp:consume queue [number] [prefetch]
+
+Will process messages from the given queue
+USAGE;
+
+        $this->assertSame(
+            $expected,
+            (string) new Consume(
+                $this->createMock(Client::class),
+                new Consumers
+            )
+        );
+    }
+
+    public function testInvokation()
+    {
+        $command = new Consume(
+            $client = $this->createMock(Client::class),
+            new Consumers(
+                (new Map('string', 'callable'))
+                    ->put('foo', $expected = function(){})
+            )
+        );
+        $client
+            ->expects($this->once())
+            ->method('channel')
+            ->willReturn($channel = $this->createMock(Channel::class));
+        $channel
+            ->expects($this->once())
+            ->method('basic')
+            ->willReturn($basic = $this->createMock(Basic::class));
+        $basic
+            ->expects($this->once())
+            ->method('consume')
+            ->with($this->callback(function($consume): bool {
+                return $consume->queue() === 'foo';
+            }))
+            ->willReturn($consumer = $this->createMock(Basic\Consumer::class));
+        $consumer
+            ->expects($this->once())
+            ->method('foreach')
+            ->with($expected);
+        $env = $this->createMock(Environment::class);
+        $env
+            ->expects($this->never())
+            ->method('exit');
+        $env
+            ->expects($this->never())
+            ->method('output');
+
+        $this->assertNull($command(
+            $env,
+            new Arguments((new Map('string', 'mixed'))->put('queue', 'foo')),
+            new Options
+        ));
+    }
+
+    public function testConsumeXMessages()
+    {
+        $command = new Consume(
+            $client = $this->createMock(Client::class),
+            new Consumers(
+                (new Map('string', 'callable'))
+                    ->put('foo', $expected = function(){})
+            )
+        );
+        $client
+            ->expects($this->once())
+            ->method('channel')
+            ->willReturn($channel = $this->createMock(Channel::class));
+        $channel
+            ->expects($this->once())
+            ->method('basic')
+            ->willReturn($basic = $this->createMock(Basic::class));
+        $basic
+            ->expects($this->once())
+            ->method('qos')
+            ->with($this->callback(static function($qos): bool {
+                return $qos->prefetchCount() === 42;
+            }));
+        $basic
+            ->expects($this->once())
+            ->method('consume')
+            ->with($this->callback(function($consume): bool {
+                return $consume->queue() === 'foo';
+            }))
+            ->willReturn($consumer = $this->createMock(Basic\Consumer::class));
+        $consumer
+            ->expects($this->once())
+            ->method('take')
+            ->with(42)
+            ->will($this->returnSelf());
+        $consumer
+            ->expects($this->once())
+            ->method('foreach')
+            ->with($expected);
+        $env = $this->createMock(Environment::class);
+        $env
+            ->expects($this->never())
+            ->method('exit');
+        $env
+            ->expects($this->never())
+            ->method('output');
+
+        $this->assertNull($command(
+            $env,
+            new Arguments(
+                (new Map('string', 'mixed'))
+                    ->put('queue', 'foo')
+                    ->put('number', '42')
+            ),
+            new Options
+        ));
+    }
+
+    public function testPrefetchMessages()
+    {
+        $command = new Consume(
+            $client = $this->createMock(Client::class),
+            new Consumers(
+                (new Map('string', 'callable'))
+                    ->put('foo', $expected = function(){})
+            )
+        );
+        $client
+            ->expects($this->once())
+            ->method('channel')
+            ->willReturn($channel = $this->createMock(Channel::class));
+        $channel
+            ->expects($this->once())
+            ->method('basic')
+            ->willReturn($basic = $this->createMock(Basic::class));
+        $basic
+            ->expects($this->once())
+            ->method('qos')
+            ->with($this->callback(static function($qos): bool {
+                return $qos->prefetchCount() === 24;
+            }));
+        $basic
+            ->expects($this->once())
+            ->method('consume')
+            ->with($this->callback(function($consume): bool {
+                return $consume->queue() === 'foo';
+            }))
+            ->willReturn($consumer = $this->createMock(Basic\Consumer::class));
+        $consumer
+            ->expects($this->once())
+            ->method('take')
+            ->with(42)
+            ->will($this->returnSelf());
+        $consumer
+            ->expects($this->once())
+            ->method('foreach')
+            ->with($expected);
+        $env = $this->createMock(Environment::class);
+        $env
+            ->expects($this->never())
+            ->method('exit');
+        $env
+            ->expects($this->never())
+            ->method('output');
+
+        $this->assertNull($command(
+            $env,
+            new Arguments(
+                (new Map('string', 'mixed'))
+                    ->put('queue', 'foo')
+                    ->put('number', '42')
+                    ->put('prefetch', '24')
+            ),
+            new Options
+        ));
+    }
+}
