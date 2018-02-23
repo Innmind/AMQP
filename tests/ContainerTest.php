5b7df64c889b1df3df975c2c6d4b641531ac6be2
Baptiste Langlade <baptouuuu@gmail.com>
2018-02-23 08:38:21 +0100

    test commands are correctly built via the container

--- tests/ContainerTest.php
+++ tests/ContainerTest.php
@@ -3,9 +3,12 @@ declare(strict_types = 1);
 
 namespace Tests\Innmind\AMQP;
 
-use Innmind\AMQP\Client\{
-    Client,
-    SignalAware
+use Innmind\AMQP\{
+    Client\Client,
+    Client\SignalAware,
+    Command\Get,
+    Command\Purge,
+    Command\Consume,
 };
 use Innmind\Compose\{
     ContainerBuilder\ContainerBuilder,
@@ -40,5 +43,8 @@ class ContainerTest extends TestCase
 
         $this->assertInstanceOf(Client::class, $container->get('basic'));
         $this->assertInstanceOf(SignalAware::class, $container->get('client'));
+        $this->assertInstanceOf(Get::class, $container->get('getCommand'));
+        $this->assertInstanceOf(Purge::class, $container->get('purgeCommand'));
+        $this->assertInstanceOf(Consume::class, $container->get('consumeCommand'));
     }
 }
