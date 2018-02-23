6611ce21fb1e0d4a1ecfd85240db10175bf686d0
Baptiste Langlade <baptouuuu@gmail.com>
2018-02-23 08:35:29 +0100

    add get command

---
+++ tests/Command/GetTest.php
@@ -0,0 +1,94 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\AMQP\Command;
+
+use Innmind\AMQP\{
+    Command\Get,
+    Client,
+    Client\Channel,
+    Client\Channel\Basic,
+    Consumers,
+};
+use Innmind\CLI\{
+    Command,
+    Command\Arguments,
+    Command\Options,
+    Environment,
+};
+use Innmind\Immutable\Map;
+use PHPUnit\Framework\TestCase;
+
+class GetTest extends TestCase
+{
+    public function testInterface()
+    {
+        $this->assertInstanceOf(
+            Command::class,
+            new Get(
+                $this->createMock(Client::class),
+                new Consumers
+            )
+        );
+    }
+
+    public function testDefinition()
+    {
+        $expected = <<<USAGE
+innmind:amqp:get queue
+
+Will process a single message from the given queue
+USAGE;
+
+        $this->assertSame(
+            $expected,
+            (string) new Get(
+                $this->createMock(Client::class),
+                new Consumers
+            )
+        );
+    }
+
+    public function testInvokation()
+    {
+        $command = new Get(
+            $client = $this->createMock(Client::class),
+            new Consumers(
+                (new Map('string', 'callable'))
+                    ->put('foo', $expected = function(){})
+            )
+        );
+        $client
+            ->expects($this->once())
+            ->method('channel')
+            ->willReturn($channel = $this->createMock(Channel::class));
+        $channel
+            ->expects($this->once())
+            ->method('basic')
+            ->willReturn($basic = $this->createMock(Basic::class));
+        $basic
+            ->expects($this->once())
+            ->method('get')
+            ->with($this->callback(function($get): bool {
+                return $get->queue() === 'foo';
+            }))
+            ->willReturn($get = $this->createMock(Basic\Get::class));
+        $get
+            ->expects($this->once())
+            ->method('__invoke')
+            ->with($expected);
+        $env = $this->createMock(Environment::class);
+        $env
+            ->expects($this->never())
+            ->method('exit');
+        $env
+            ->expects($this->never())
+            ->method('output');
+
+        $this->assertNull($command(
+            $env,
+            new Arguments((new Map('string', 'mixed'))->put('queue', 'foo')),
+            new Options
+        ));
+    }
+}
