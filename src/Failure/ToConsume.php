b3624d33bc1fe719ae60b1fb2fe0452a672d2a42
Baptiste Langlade <baptiste.langlade@hey.com>
2025-08-08 20:20:40 +0200

    remove useless code

--- src/Failure/ToConsume.php
+++ src/Failure/ToConsume.php
@@ -3,9 +3,7 @@ declare(strict_types = 1);
 
 namespace Innmind\AMQP\Failure;
 
-use Innmind\AMQP\{
-    Model\Basic\Consume as Command,
-};
+use Innmind\AMQP\Model\Basic\Consume as Command;
 
 /**
  * @psalm-immutable
@@ -26,11 +24,5 @@ final class ToConsume
     public function command(): Command
     {
         return $this->command;
-    }
-
-    #[\NoDiscard]
-    public function kind(): Kind
-    {
-        return Kind::toConsume;
     }
 }
