2375fc5fc4fa5ec3379d113da98486c8593aec07
Baptiste Langlade <baptiste.langlade@hey.com>
2026-02-22 15:24:53 +0100

    better handle errors when installing signals listener

--- src/Transport/Connection.php
+++ src/Transport/Connection.php
@@ -211,9 +211,12 @@ final class Connection
             ));
     }
 
-    public function listenSignals(Signals $signals, Channel $channel): void
+    /**
+     * @return Attempt<SideEffect>
+     */
+    public function listenSignals(Signals $signals, Channel $channel): Attempt
     {
-        $this->signals->install($signals, $channel);
+        return $this->signals->install($signals, $channel);
     }
 
     private function flagActive(ReceivedFrame $received): ReceivedFrame
